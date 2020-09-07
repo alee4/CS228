1bd8329f1bbc449b0566c4820409b35e60916316
austin <austin.lee@uvm.edu>
2020-09-06 20:41:46 -0400

    completed up to part 48

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,8 +1,11 @@
 
 var controllerOptions = {};
 var i = 0;
+var x = (window.innerWidth)/2;
+var y = (window.innerHeight)/2;
 
 Leap.loop(controllerOptions, function(frame) {
     console.log(i)
+    circle(x,y,100);
     }
 );
