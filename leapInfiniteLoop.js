479c51de1b16d2d6218a0309822c85c0080967a8
austin <austin.lee@uvm.edu>
2020-09-06 21:02:15 -0400

    completed up to part 52, random circle complete

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,11 +1,17 @@
 
 var controllerOptions = {};
-var i = 0;
+//var i = 0;
 var x = (window.innerWidth)/2;
 var y = (window.innerHeight)/2;
+var ranNumX;
+var ranNumY;
 
 Leap.loop(controllerOptions, function(frame) {
-    console.log(i)
-    circle(x,y,100);
+
+    clear();
+    ranNumX = Math.floor(Math.random() * 2) -1;
+    ranNumY = Math.floor(Math.random() * 2) -1;
+    //console.log(i)
+    circle(x + ranNumX,y + ranNumY,100);
     }
 );
