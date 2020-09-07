bf6e80d40f3f7ba4938a719929328a7a374278ec
austin <austin.lee@uvm.edu>
2020-09-06 22:05:30 -0400

    completed up to part 58, shows fingers array

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,14 +5,22 @@ var x = (window.innerWidth)/2;
 var y = (window.innerHeight)/2;
 var ranNumX;
 var ranNumY;
+var hand;
+var fingers;
 
 Leap.loop(controllerOptions, function(frame) {
 
     //var arr = new Array(frame.hands);
     // //console.log(frame);
     if (frame.hands.length === 1){
-        console.log(frame.hands)
+        //console.log(frame.hands[0]);
+        hand = frame.hands[0];
+        //console.log(hand);
+        fingers = hand.fingers;
+        console.log(fingers);
     }
+
+
 
     // clear();
     // ranNumX = Math.floor(Math.random() * 2) -1;
