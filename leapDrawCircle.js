ba0ff2beb28af73932f87d94a44f960778e4717d
austin <austin.lee@uvm.edu>
2020-09-14 19:22:59 -0400

    finished up to part 16 of deliverable 2

--- leapDrawCircle.js
+++ leapDrawCircle.js
@@ -1,6 +1,6 @@
     var controllerOptions = {};
     var x = 0;
-    var y = 0
+    var y = 0;
     var z = 0;
     var rawXMin = 1500;
     var rawXMax = 0;
@@ -24,35 +24,66 @@
 
     function HandleHand(hand){
         var finger = hand.fingers;
-        HandleFinger(finger)
+        finger.forEach(function(finger){
+            HandleFinger(finger)
+        });
     }
 
     function HandleFinger(finger) {
-        finger.forEach(function(finger){
-        //if (finger.type == 1){
-            //console.log("Index finger info: " + finger);
+        var bone = finger.bones;
+        bone.forEach(function(bone) {
+            //if (finger.type == 1){
+            //console.log(bone);
+            HandleBone(bone);
 
-            x = finger.tipPosition[0];
-            y =  window.innerHeight-finger.tipPosition[1]; //finger.tipPosition[1];
-            z = finger.tipPosition[2];
-
-            if (x < rawXMin){
-                rawXMin = x;
-            }
-            if ( x > rawXMax){
-                rawXMax = x;
-            }
-            if (y < rawYMin){
-                rawYMin = y;
-            }
-            if ( y > rawYMax){
-                rawYMax = y;
-            }
-
-            outputX =  ((x - rawXMin) / (rawXMax - rawXMin)) * (window.innerWidth - 0);
-            outputY =  ((y - rawYMin) / (rawYMax - rawYMin)) * (window.innerHeight - 0);
-
-            circle(outputX, outputY, 100);
-        //}
+            // x = finger.tipPosition[0];
+            // y = window.innerHeight - finger.tipPosition[1]; //finger.tipPosition[1];
+            // z = finger.tipPosition[2];
+            //
+            // if (x < rawXMin) {
+            //     rawXMin = x;
+            // }
+            // if (x > rawXMax) {
+            //     rawXMax = x;
+            // }
+            // if (y < rawYMin) {
+            //     rawYMin = y;
+            // }
+            // if (y > rawYMax) {
+            //     rawYMax = y;
+            // }
+            //
+            // outputX = ((x - rawXMin) / (rawXMax - rawXMin)) * (window.innerWidth - 0);
+            // outputY = ((y - rawYMin) / (rawYMax - rawYMin)) * (window.innerHeight - 0);
+            //
+            // circle(outputX, outputY, 100);
+            //}
         });
+    }
+
+    function HandleBone(bone){
+        x = bone.nextJoint[0];
+        y = window.innerHeight - bone.nextJoint[1];
+        z = bone.nextJoint[2];
+
+        //console.log(x,y,z)
+
+        if (x < rawXMin) {
+            rawXMin = x;
+        }
+        if (x > rawXMax) {
+            rawXMax = x;
+        }
+        if (y < rawYMin) {
+            rawYMin = y;
+        }
+        if (y > rawYMax) {
+            rawYMax = y;
+        }
+
+        outputX = ((x - rawXMin) / (rawXMax - rawXMin)) * (window.innerWidth - 0);
+        outputY = ((y - rawYMin) / (rawYMax - rawYMin)) * (window.innerHeight - 0);
+
+        circle(outputX, outputY, 100);
+
     }
